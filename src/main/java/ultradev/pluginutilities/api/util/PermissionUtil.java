82c494556de964562464d5b23029cd0412644cdf
SkyblockSquad <ultra.alt@outlook.com>
2021-07-18 19:05:10 +0200

    PermissionUtil

---
+++ src/main/java/ultradev/pluginutilities/api/util/PermissionUtil.java
@@ -0,0 +1,12 @@
+package ultradev.pluginutilities.api.util;
+
+import org.bukkit.entity.Player;
+import ultradev.pluginutilities.Main;
+
+public class PermissionUtil {
+
+    public static boolean hasPermission(Player player, String permission) {
+        return player.hasPermission(Main.pluginName.toLowerCase() + "." + "permission");
+    }
+
+}
